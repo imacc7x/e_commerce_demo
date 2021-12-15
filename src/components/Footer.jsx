b1d3c147e2f2a4087c1cfca50cd2743359491c6c
Siripong Chusri <siripong.chu@ku.th>
2021-12-15 19:32:08 +0700

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,135 @@
+import {
+  Facebook,
+  Instagram,
+  MailOutline,
+  Phone,
+  Room,
+  Twitter,
+} from "@material-ui/icons";
+import React from "react";
+import styled from "styled-components";
+import { mobile } from "../responsive";
+
+const Container = styled.div`
+  display: flex;
+  ${mobile({ flexDirection: "column" })}
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const Logo = styled.h1``;
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+const SocialContainer = styled.div`
+  display: flex;
+`;
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+  ${mobile({ display: "none" })}
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+  ${mobile({ backgroundColor:'#fff8f8' })}
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: "center";
+`;
+const Payment = styled.img`
+  width: 50%;
+`;
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>Alisa7x</Logo>
+        <Desc>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi
+          mollitia atque, autem nemo molestiae odio optio veritatis veniam velit
+          magnam officia doloribus sapiente asperiores sint natus libero.
+          Maxime, dolorem adipisci!
+        </Desc>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <Facebook></Facebook>
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <Instagram></Instagram>
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter></Twitter>
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Term</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room style={{ marginRight: "10px" }} />
+          813
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{ marginRight: "10px" }} />
+          +66 69 22 6 33
+        </ContactItem>
+        <ContactItem>
+          <MailOutline style={{ marginRight: "10px" }} />
+          contact@Alisa7.x
+        </ContactItem>
+        <Payment />
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;
---
+++ src/data.js
@@ -0,0 +1,75 @@
+import girl1 from "../src/images/girl1.png";
+import girl2 from "../src/images/girl2.png";
+import shirt1 from "../src/images/shirt1.jpeg";
+import shirt2 from "../src/images/shirt2.jpeg";
+import shirt3 from "../src/images/shirt3.jpeg";
+
+export const sliderItem = [
+  {
+    id: 1,
+    img: girl1,
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: girl2,
+    title: "WINTER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: shirt1,
+    title: "SHIRT STYLE!",
+  },
+  {
+    id: 2,
+    img: shirt2,
+    title: "LOUNGEWEAR LOVE",
+  },
+  {
+    id: 3,
+    img: shirt3,
+    title: "LIGHT JACKET",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: shirt1,
+  },
+  {
+    id: 2,
+    img: girl2,
+  },
+  {
+    id: 3,
+    img: girl2,
+  },
+  {
+    id: 4,
+    img: shirt1,
+  },
+  {
+    id: 5,
+    img: girl2,
+  },
+  {
+    id: 6,
+    img: girl2,
+  },
+  {
+    id: 7,
+    img: shirt1,
+  },
+  {
+    id: 8,
+    img: girl2,
+  },
+];
